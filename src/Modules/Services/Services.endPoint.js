3d32d1a0bd81596ce1b5bd71326946512b041bde
Nada Obaid <nada.s.obaidd@gmail.com>
2023-12-02 00:00:39 +0200

    some updateNow

--- src/Modules/Services/Services.endPoint.js
+++ src/Modules/Services/Services.endPoint.js
@@ -1,8 +1,8 @@
 import { roles } from "../../MiddelWare/Auth.js"
 
 export const endPoint = {
-getServices: [roles.Admin],
-getDetailsServices: [roles.Admin],
+getServices: [roles.User],
+getDetailsServices: [roles.User],
 create: [roles.Admin],
 update: [roles.Admin],
 softDelete: [roles.Admin],
