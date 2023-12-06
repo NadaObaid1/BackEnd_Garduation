a705b78505d7f27f096f7e4a41571a703cb5a0c1
Ghadeer Hamad <ghadeerohamad2001@gmail.com>
2023-12-06 20:48:48 +0200

    hggh

--- src/Modules/Problem/Problem.Controller.js
+++ src/Modules/Problem/Problem.Controller.js
@@ -1,10 +1,10 @@
 
 import ProblemModel from "../../../DB/Model/Problem.Model.js"
-
+//, userID: req.user._id
 
 export const createProblem = async(req, res)=>{
     const {problem} = req.body
-    const createProb = await ProblemModel.create({problem, userID: req.user._id})
+    const createProb = await ProblemModel.create({problem})
     return res.status(201).json({message: "success", createProb})
     
     
--- src/Modules/Problem/Problem.Router.js
+++ src/Modules/Problem/Problem.Router.js
@@ -3,10 +3,10 @@ import * as userController from "./Problem.Controller.js"
 import {asynHandler} from '../../Services/errorHandler.js'
 import validation from '../../Services/Validation.js'
 import { ProblemSchema } from './Problem.Validation.js'
-import { auth } from '../../Middlware/Auth.js'
-
+//import { auth } from '../../Middlware/Auth.js'
+// auth(["Admin"]) ,
 const router = Router()
 
-router.post("/problem", auth(["Admin"]) ,validation(ProblemSchema), asynHandler(userController.createProblem));
+router.post("/problem", validation(ProblemSchema), asynHandler(userController.createProblem));
 
 export default router;
