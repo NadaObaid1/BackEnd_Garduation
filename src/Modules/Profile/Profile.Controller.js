2c69ca3c349f9b8a5a18c1a8fec98826f7a99e23
Ghadeer Hamad <ghadeerohamad2001@gmail.com>
2023-12-22 21:46:00 +0200

    hgf

--- src/Modules/Profile/Profile.Controller.js
+++ src/Modules/Profile/Profile.Controller.js
@@ -1,20 +1,24 @@
 import UserModel from "../../../DB/Model/User.Model.js";
 
-
 export const updateProfile = async (req, res) => {
-    try {
-        const {userName, age, phone, address} = req.body;
-      const updatedProfile = await UserModel.findByIdAndUpdate(
-        req.params.id,
-        {userName, age, phone, address},
-        { new: true }
-      );
-      if (updatedProfile) {
-        res.status(200).json(updatedProfile);
-      } else {
-        res.status(404).json({ message: 'Post not found' });
-      }
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+  try {
+    const { userName, age, phone, address } = req.body;
+
+    // Validate the incoming data here if needed
+
+    const updatedProfile = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      { userName, age, phone, address },
+      { new: true }
+    );
+
+    if (updatedProfile) {
+      res.status(200).json(updatedProfile);
+    } else {
+      res.status(404).json({ message: 'Profile not found' });
     }
-  };
+  } catch (error) {
+    console.error('Error updating profile:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
