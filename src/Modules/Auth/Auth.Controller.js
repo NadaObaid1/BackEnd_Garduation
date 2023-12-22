e611c969ea286a436de3975f2af0ee833496966c
Ghadeer Hamad <ghadeerohamad2001@gmail.com>
2023-12-22 20:36:24 +0200

    hh

--- src/Modules/Auth/Auth.Controller.js
+++ src/Modules/Auth/Auth.Controller.js
@@ -89,7 +89,7 @@ export const SignIn = async(req, res, next)=>{
 
     const token = jwt.sign({id:user._id, role: user.role}, process.env.LOGINSINGURE, {expiresIn: '1h'})//عشان موضوع السيكورتي وما يصير في تعديل ع الداتا 
     const refreshToken = jwt.sign({id:user._id, role: user.role}, process.env.LOGINSINGURE, {expiresIn:  60*60*24*30})
-    return res.status(200).json({message: "success", token, refreshToken  , id:user._id,role: user.role,name: user.userName, age: user.age, phone: user.phone, address: user.address});
+    return res.status(200).json({message: "success", token, refreshToken  , id:user._id,role: user.role,name: user.userName, email: user.email, age: user.age, phone: user.phone, address: user.address});
 }
 
 export const ConfirmEmail = async(req, res)=>{
