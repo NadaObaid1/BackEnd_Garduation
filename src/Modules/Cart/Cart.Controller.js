8a667d1b4111631a738ccccb3ac578c8906de09c
Nada Obaid <nada.s.obaidd@gmail.com>
2024-01-06 18:01:08 +0200

    add some update

--- src/Modules/Cart/Cart.Controller.js
+++ src/Modules/Cart/Cart.Controller.js
@@ -3,21 +3,25 @@ import cartModel from '../../../DB/Model/Cart.Model.js'
 export const CreateCart = async (req, res) => {
     try {
         const { productId, quantity } = req.body;
-        const cart = await cartModel.findOne({ userId: req.user._id }).populate('products.productId', 'name image');
+        const cart = await cartModel.findOne({ userId: req.user._id }).populate({
+            path: 'products.productId',
+            select: 'name image',
+        });
 
         if (!cart) {
             const newCart = await cartModel.create({
                 userId: req.user._id,
                 products: [{ productId, quantity }]
-            });
+            })
 
-            //return res.status(201).json({ message: "success", newCart });
+            console.log("New Cart created:", newCart);
+            return res.status(201).json({ message: "success", cart: newCart });
         }
 
         let matchedProduct = false;
 
         for (let i = 0; i < cart.products.length; i++) {
-            if (cart.products[i].productId == productId) {
+            if (cart.products[i].productId._id.equals(productId)) { // Use equals to compare ObjectId
                 cart.products[i].quantity = quantity;
                 matchedProduct = true;
                 break;
@@ -27,13 +31,10 @@ export const CreateCart = async (req, res) => {
         if (!matchedProduct) {
             cart.products.push({ productId, quantity });
         }
-
         await cart.save();
+        console.log("Cart updated:", cart);
 
-        // Populate product details again for the response
-        const populatedCart = await cartModel.findOne({ userId: req.user._id }).populate('products.productId', 'name image');
-
-        return res.status(201).json({ message: "success", cart: populatedCart });
+        return res.status(201).json({ message: "success", cart });
     } catch (error) {
         console.error('Error adding product to cart:', error);
         return res.status(500).json({ message: 'Internal server error' });
