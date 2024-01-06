d4ea6151a2537186490aa729884791bf56d8ff0f
Nada Obaid <nada.s.obaidd@gmail.com>
2024-01-06 20:18:51 +0200

    add some update

--- src/Modules/Cart/Cart.Router.js
+++ src/Modules/Cart/Cart.Router.js
@@ -9,7 +9,7 @@ router.post('/',auth(endPoint.create), CartController.CreateCart);
 router.patch('/removeItem',auth(endPoint.delete), CartController.removeItem);
 router.patch('/clearCart',auth(endPoint.clear), CartController.clearCart);
 router.get('/getCart',auth(endPoint.get), CartController.getCart);
-router.post('/increaseQuantity',auth(endPoint.get), CartController.increaseQuantity);
-router.post('/decreaseQuantity',auth(endPoint.get), CartController.decreaseQuantity);
+router.put('/increaseQuantity',auth(endPoint.get), CartController.increaseQuantity);
+router.put('/decreaseQuantity',auth(endPoint.get), CartController.decreaseQuantity);
 
 export default router
