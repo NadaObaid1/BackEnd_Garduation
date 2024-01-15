27a50271ce3eba34213f43f52267599935eeba92
Ghadeer Hamad <ghadeerohamad2001@gmail.com>
2024-01-15 14:35:01 +0200

    hh

--- src/Modules/Salons/Salon.Router.js
+++ src/Modules/Salons/Salon.Router.js
@@ -3,7 +3,7 @@ import * as salonController from "./Salon.Controller.js";
 import ProductRouter from "../Products/Products.Router.js"
 import PostRouter from "../Post/Post.Router.js"
 import JobRouter from "../Job/Job.Router.js"
-import {auth} from "../../Middlware/Auth.js"
+import {auth, roles} from "../../Middlware/Auth.js"
 
 import fileUpload, {fileValidation} from "../../Services/multer.js"
 
@@ -15,7 +15,7 @@ router.use("/:id/Post", PostRouter)
 router.use("/:id/Job", JobRouter)
 
 router.post('/salon',fileUpload(fileValidation.image).single('image'), salonController.createSalon);
-router.get('/salon', auth() ,salonController.getAllSalons);
+router.get('/salon', auth([roles.Admin, roles.User]) ,salonController.getAllSalons);
 router.get('/salon/:id', salonController.getSalonById);
 router.put('/salon/:id', salonController.updateSalon);
 router.delete('/salon/:id', salonController.deleteSalon);
