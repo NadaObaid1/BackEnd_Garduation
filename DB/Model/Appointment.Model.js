83d830e1b4799018cb0c35e12d7dcaf250d8c40d
Maymonakh <maymonakhanfar@gmail.com>
2024-01-18 19:53:16 +0200

    add branches

--- DB/Model/Appointment.Model.js
+++ DB/Model/Appointment.Model.js
@@ -14,6 +14,10 @@ const AppointmentSchema = new Schema(
       type: String,
       required: true,
     },
+    branch: {
+      type: String,
+      required: true,
+    },
     appointment_date: {
       type: String,
       required: true,
