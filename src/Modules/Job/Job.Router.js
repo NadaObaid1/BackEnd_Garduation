ebcc678813a6eabbf5235f5f81487b05e1d0221c
Ghadeer Hamad <ghadeerohamad2001@gmail.com>
2024-01-20 21:21:23 +0200

    g

--- src/Modules/Job/Job.Router.js
+++ src/Modules/Job/Job.Router.js
@@ -9,10 +9,10 @@ import {auth, roles} from "../../Middlware/Auth.js"
 const router = Router({mergeParams: true});  
   
 
-router.post("/job", auth([roles.Admin, roles.User]),fileUpload(fileValidation.image).single('image'), jobController.createJob);
+router.post("/job", auth([roles.Admin]),fileUpload(fileValidation.image).single('image'), jobController.createJob);
 router.get("/job", auth([roles.Admin, roles.User]),jobController.getAllJobs);
 router.get('/job/:id', auth([roles.Admin, roles.User]),jobController.getJobById);
-router.put('/job/:id', auth([roles.Admin, roles.User]),jobController.updateJob);
-router.delete('/job/:id', auth([roles.Admin, roles.User]),jobController.deleteJob); 
+router.put('/job/:id', auth(roles.Admin),jobController.updateJob);
+router.delete('/job/:id', auth(roles.Admin),jobController.deleteJob); 
 
 export default router; 
