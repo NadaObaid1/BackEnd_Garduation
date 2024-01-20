c57543f6d46b0facd480d03f51e3a66409b9a232
Ghadeer Hamad <ghadeerohamad2001@gmail.com>
2024-01-21 00:34:42 +0200

    uploadJob token

--- src/Modules/Uploadjob/Uploadjob.Router.js
+++ src/Modules/Uploadjob/Uploadjob.Router.js
@@ -1,11 +1,13 @@
 import {Router} from 'express'
 import * as uploadjobController from "./Uploadjob.Controller.js"
 import fileUpload, {fileValidation} from "../../Services/multer.js"
+import {auth, roles} from "../../Middlware/Auth.js"
+
 
 const router = Router({mergeParams: true}); 
  
 
-router.post("/uploadjob", fileUpload(fileValidation.pdf).single('image'), uploadjobController.uploadJob);
-router.get("/uploadjob", uploadjobController.getAllJobs);
+router.post("/uploadjob", auth([roles.Admin, roles.User]),fileUpload(fileValidation.pdf).single('image'), uploadjobController.uploadJob);
+router.get("/uploadjob", auth(roles.Admin),uploadjobController.getAllJobs);
 
 export default router; 
