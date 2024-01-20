f231d5e56f30b72d2145965a789fcea779dd08a6
Ghadeer Hamad <ghadeerohamad2001@gmail.com>
2024-01-21 00:04:54 +0200

    post token

--- src/Modules/Post/Post.Router.js
+++ src/Modules/Post/Post.Router.js
@@ -1,17 +1,18 @@
 import {Router} from 'express'
 import * as postController from "./Post.Controller.js"
 import fileUpload, {fileValidation} from "../../Services/multer.js"
+import {auth, roles} from "../../Middlware/Auth.js"
 
 
 const router = Router({mergeParams: true}); 
 
 
 
-router.post("/post", fileUpload(fileValidation.image).single('image'), postController.createPost);
-router.get("/post", postController.getAllPosts);
-router.get('/post/:id', postController.getPostById);
-router.put('/post/:id', postController.updatePost);
-router.delete('/post/:id', postController.deletePost);
+router.post("/post", auth(roles.Admin),fileUpload(fileValidation.image).single('image'), postController.createPost);
+router.get("/post", auth([roles.Admin, roles.User]),postController.getAllPosts);
+router.get('/post/:id', auth([roles.Admin, roles.User]),postController.getPostById);
+router.put('/post/:id', auth(roles.Admin),postController.updatePost);
+router.delete('/post/:id', auth(roles.Admin),postController.deletePost);
 router.post('/post/:id/:Id', postController.increaseLikesController);
 
  
