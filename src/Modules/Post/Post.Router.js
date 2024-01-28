d036a36b62b2f5b0f66468c82c7a4ffbf1ad708f
Ghadeer Hamad <ghadeerohamad2001@gmail.com>
2024-01-29 01:35:57 +0200

    HHHH

--- src/Modules/Post/Post.Router.js
+++ src/Modules/Post/Post.Router.js
@@ -8,11 +8,11 @@ const router = Router({mergeParams: true});
 
 
 
-router.post("/post", auth(roles.Admin),fileUpload(fileValidation.image).single('image'), postController.createPost);
-router.get("/post", auth([roles.Admin, roles.User]),postController.getAllPosts);
-router.get('/post/:id', auth([roles.Admin, roles.User]),postController.getPostById);
-router.put('/post/:id', auth(roles.Admin),postController.updatePost);
-router.delete('/post/:id', auth(roles.Admin),postController.deletePost);
+router.post("/post", auth([roles.Admin, roles.Manager]),fileUpload(fileValidation.image).single('image'), postController.createPost);
+router.get("/post", auth([roles.Admin, roles.User, roles.Manager]),postController.getAllPosts);
+router.get('/post/:id', auth([roles.Admin, roles.User, roles.Manager]),postController.getPostById);
+router.put('/post/:id', auth([roles.Admin, roles.Manager]),postController.updatePost);
+router.delete('/post/:id', auth([roles.Admin, roles.Manager]),postController.deletePost);
 router.post('/post/:id/:Id', postController.increaseLikesController);
 
  
