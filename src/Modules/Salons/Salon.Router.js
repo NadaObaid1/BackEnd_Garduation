a74cdd3188eabfbc2da196ec9255388f91a2a253
Nada Obaid <nada.s.obaidd@gmail.com>
2024-01-29 23:56:03 +0200

    add some update

--- src/Modules/Salons/Salon.Router.js
+++ src/Modules/Salons/Salon.Router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import * as salonController from "./Salon.Controller.js";
 import ProductRouter from "../Products/Products.Router.js"
+import ServiceRouter from "../Services/Services.Router.js"
 import PostRouter from "../Post/Post.Router.js"
 import JobRouter from "../Job/Job.Router.js"
 import UploadjobRouter from "../Uploadjob/Uploadjob.Router.js"
@@ -16,6 +17,7 @@ import fileUpload, {fileValidation} from "../../Services/multer.js"
 const router = Router();
 
 router.use("/:id/Product", ProductRouter)
+router.use("/:id/services", ServiceRouter)
 router.use("/:id/Post", PostRouter)
 router.use("/:id/Job", JobRouter)
 router.use("/:id/Uploadjob", UploadjobRouter)
